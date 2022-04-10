841dd4b91f3962decafa805d52e1a525e6605ddb
Your Name <you@example.com>
2022-04-10 11:44:22 -0700

    [Megha] Refactored: Compared The Two Lines

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -5,15 +5,22 @@ namespace LineComparison
     {
         static void Main(String[] args)
         {
-            LineLength computation = new LineLength(40, 70, 93, 120);
+            LineLength computation = new LineLength(30, 65, 87, 100);
             double line1 = computation.CalculateLength();
-            LineLength line = new LineLength(44, 68, 79, 118);
+            LineLength line = new LineLength(35, 74, 82, 110);
             double line2 = line.CalculateLength();
-            if (line1.Equals(line2))
+            if (line1.CompareTo(line2) == 0)
+            {
                 Console.WriteLine("Length of both lines are equal");
+            }
+            if (line1.CompareTo(line2) > 0)
+            {
+                Console.WriteLine("Line 1 is greater");
+            }
             else
-
-                Console.WriteLine("Length of both lines are not equal");
+            {
+                Console.WriteLine("Line 2 is greater");
+            }
 
         }
     }
