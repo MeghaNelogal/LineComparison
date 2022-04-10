51e9e0add3cf9b122eb8b970cba360d385979b28
Your Name <you@example.com>
2022-04-10 11:32:33 -0700

    [Megha] Add: Equality Of Lines Is Checked

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -5,8 +5,16 @@ namespace LineComparison
     {
         static void Main(String[] args)
         {
-            LineLength computation = new LineLength(30, 72, 81, 105);
-            computation.CalculateLength();
+            LineLength computation = new LineLength(40, 70, 93, 120);
+            double line1 = computation.CalculateLength();
+            LineLength line = new LineLength(44, 68, 79, 118);
+            double line2 = line.CalculateLength();
+            if (line1.Equals(line2))
+                Console.WriteLine("Length of both lines are equal");
+            else
+
+                Console.WriteLine("Length of both lines are not equal");
+
         }
     }
 }
